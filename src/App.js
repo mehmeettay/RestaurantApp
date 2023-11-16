33a1c8240ecea9c84d71eaf725b44ca3b18612ef
mehmeettay <memookayy@gmail.com>
2023-11-16 22:53:03 +0300

    Added Contact page

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Home from './pages/Home';
 import Footer from './components/Footer';
 import Menu from './pages/Menu';
 import About from './pages/About';
+import Contact from './pages/Contact';
 
 import {  Route, Router, Routes} from 'react-router-dom';
+
 
 function App() {
   return (
@@ -17,6 +19,7 @@ function App() {
         <Route path='/' element={<Home/>}/>
         <Route path='/menu' element={<Menu/>}/>
         <Route path='/about' element={<About/>}/>
+        <Route path='/contact' element={<Contact/>}/>
         
 
       </Routes>
