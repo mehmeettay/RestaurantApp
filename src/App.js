bd453d060e3a254b80fc4c5ead42655e92bf2460
mehmeettay <memookayy@gmail.com>
2023-11-16 22:10:05 +0300

    Added About page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import Menu from './pages/Menu';
+import About from './pages/About';
 
 import {  Route, Router, Routes} from 'react-router-dom';
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/menu' element={<Menu/>}/>
+        <Route path='/about' element={<About/>}/>
         
 
       </Routes>
